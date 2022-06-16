cde356e5aeec9d1fba1928f90d65e361df4b64de
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-16 15:00:14 +0100

    employee constructor function

--- src/lib/Employee.js
+++ src/lib/Employee.js
@@ -1,5 +1,19 @@
 //parent class
 
-class Employee {}
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+
+  getName() {}
+
+  getId() {}
+
+  getEmail() {}
+
+  getRole() {}
+}
 
 module.exports = Employee;
