a7c47fe9827d22e6e67d62e9ad7c5959d2481109
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-17 16:23:54 +0100

    terminal thinks my answers are commands

--- src/index.js
+++ src/index.js
@@ -2,34 +2,30 @@ const inquirer = require("inquirer");
 
 const fs = require("fs");
 
-const basicQuestions = () => {
-    const questions = [
-        {
-            type: "input",
-            message: "What is the employee's name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is the employee's ID?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is the employee's email address?",
-            name: "email",
-        },
-    ];
+const questions = [
+  {
+    type: "input",
+    message: "What is the employee's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the employee's ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the employee's email address?",
+    name: "email",
+  },
+];
 
-}
-
-module.exports = {
-    basicQuestions
-}
-
-const init = async () = {
-
-    const answers = await inquirer.prompt(questions);
+const init = async () => {
+  const answers = await inquirer.prompt(questions);
 };
 
 init();
+
+module.exports = {
+  questions,
+};
