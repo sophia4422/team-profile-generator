bdabbc8aae305c89919f22ef82ce8c91a99207cf
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-17 23:23:41 +0100

    employee test

--- src/lib/Employee.test.js
+++ src/lib/Employee.test.js
@@ -1,7 +1,37 @@
-describe("", () => {
-  //tests block goes here
-  it("", () => {
-      const expected =
-      const actual =
+// describe("", () => {
+//   //tests block goes here
+//   it("", () => {
+//       const expected =
+//       const actual =
+//   });
+// });
+
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+  const mockName = "Bob";
+  const mockId = "123456";
+  const mockEmail = "sophia@email.com";
+
+  const employee = new Employee(mockName, mockId, mockEmail);
+
+  it("should be an instance of Employee", () => {
+    expect(employee).toBeInstanceOf(Employee);
+  });
+
+  it("should return the expected name", () => {
+    expect(employee.getName()).toEqual(mockName);
+  });
+
+  it("should return the expected id", () => {
+    expect(employee.getId()).toEqual(mockId);
+  });
+
+  it("should return the expected email", () => {
+    expect(employee.getEmail()).toEqual(mockEmail);
+  });
+
+  it("should return the role of Employee", () => {
+    expect(employee.getRole()).toEqual("Employee");
   });
 });
